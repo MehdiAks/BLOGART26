edee4d1bad569215231a952b14c521f0025b4b82
MehdiAks <62901138+MehdiAks@users.noreply.github.com>
2026-01-29 12:06:40 +0100

    Improve database connection guidance

--- functions/query/connect.php
+++ functions/query/connect.php
@@ -5,12 +5,18 @@ function sql_connect(){
 
     //connect BDD with PDO using SQL_HOST, SQL_USER, SQL_PWD, SQL_DB
     // Avec encodage UTF8
-    if (strpos($_SERVER['HTTP_HOST'], 'scalingo') !== false) {
-        $DB = new PDO('mysql:host=' . SQL_HOST . ';charset=utf8;dbname=' . SQL_DB . ';port='. SQL_PORT, SQL_USER, SQL_PWD);
-    } else {
-        $DB = new PDO('mysql:host=' . SQL_HOST . ';charset=utf8;dbname=' . SQL_DB, SQL_USER, SQL_PWD);
+    try {
+        if (strpos($_SERVER['HTTP_HOST'], 'scalingo') !== false) {
+            $DB = new PDO('mysql:host=' . SQL_HOST . ';charset=utf8;dbname=' . SQL_DB . ';port=' . SQL_PORT, SQL_USER, SQL_PWD);
+        } else {
+            $DB = new PDO('mysql:host=' . SQL_HOST . ';charset=utf8;dbname=' . SQL_DB, SQL_USER, SQL_PWD);
+        }
+    } catch (PDOException $exception) {
+        throw new RuntimeException(
+            'Database connection failed. Check your .env DB_HOST/DB_USER/DB_PASSWORD/DB_DATABASE values or update your local MySQL user/password.',
+            0,
+            $exception
+        );
     }
-
-    
 
 }
