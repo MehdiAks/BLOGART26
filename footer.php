1ad1a3e48f72ff0d35ffc699e30ae0da42ca7b03
MehdiAks <afantastik041@gmail.com>
2026-01-30 15:13:52 +0100

    Refactor footer structure and fix iframe syntax

--- footer.php
+++ footer.php
@@ -1,28 +1,32 @@
-    <footer class="footer" id="Contact">
-        <div class="footer-left">
-            <h3>Contactez-nous :</h3>
-            <h4>secretariat@bec-bordeaux</h4>
-            <h4>06 71 94 23 80 - 05 56 91 83 50</h4>
-            <a href="https://www.instagram.com/becfootballclub/?hl=fr" class="social-icon">
-                <img src="<?php echo ROOT_URL . '/Romain/assets/images/instagram.png'; ?>" alt="Instagram">
-            </a>
-            <a href="https://www.facebook.com/becofficiel/?locale=fr_FR" class="social-icon">
-                <img src="<?php echo ROOT_URL . '/Romain/assets/images/facebook.png'; ?>" alt="Facebook">
-            </a>
-            <h4>
-                <a href="<?php echo ROOT_URL . '/infoleg/cgu.php'; ?>">Conditions d’utilisation</a>
-            </h4>
-            <h4>
-                <a href="<?php echo ROOT_URL . '/infoleg/mentionleg.php'; ?>">Mentions légales</a>
-            </h4>
-                        <h4>
-                <a href="<?php echo ROOT_URL . '/infoleg/rgpd.php'; ?>">RGPD</a>
-            </h4>
-        </div>
+<footer class="footer" id="Contact">
+    <div class="footer-left">
+        <h3>Contactez-nous :</h3>
+        <h4>secretariat@bec-bordeaux</h4>
+        <h4>06 71 94 23 80 - 05 56 91 83 50</h4>
+        <a href="https://www.instagram.com/becfootballclub/?hl=fr" class="social-icon">
+            <img src="<?php echo ROOT_URL . '/Romain/assets/images/instagram.png'; ?>" alt="Instagram">
+        </a>
+        <a href="https://www.facebook.com/becofficiel/?locale=fr_FR" class="social-icon">
+            <img src="<?php echo ROOT_URL . '/Romain/assets/images/facebook.png'; ?>" alt="Facebook">
+        </a>
+        <h4>
+            <a href="<?php echo ROOT_URL . '/infoleg/cgu.php'; ?>">Conditions d’utilisation</a>
+        </h4>
+        <h4>
+            <a href="<?php echo ROOT_URL . '/infoleg/mentionleg.php'; ?>">Mentions légales</a>
+        </h4>
+        <h4>
+            <a href="<?php echo ROOT_URL . '/infoleg/rgpd.php'; ?>">RGPD</a>
+        </h4>
+    </div>
 
-        <div class="footer-right">
-            <img src="<?php echo ROOT_URL . '/Romain/assets/images/map.png'; ?>" class="map" alt="Carte">
-        </div>
-    </footer>
+    <div class="footer-right">
+        <<iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2829.921341579605!2d-0.5763837240139745!3d44.82316707598102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5527ba88e01e2d%3A0xf8bc9e94e84c6e71!2s51%20Rue%20Pauline%20Kergomard%2C%2033800%20Bordeaux!5e0!3m2!1sfr!2sfr!4v1769782356902!5m2!1sfr!2sfr"
+            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>>
+    </div>
+</footer>
 </body>
+
 </html>
