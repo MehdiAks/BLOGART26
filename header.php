bd46dffd2de5c9a92b8c8222566bf1d538a733e2
MehdiAks <62901138+MehdiAks@users.noreply.github.com>
2026-01-30 17:32:09 +0100

    Add actualites link to header

--- header.php
+++ header.php
@@ -263,6 +263,9 @@ $numStat = $_SESSION['numStat'] ?? null;
                     <a href="<?php echo ROOT_URL . '/index.php'; ?>">Accueil</a>
                 </li>
                 <li>
+                    <a href="<?php echo ROOT_URL . '/actualites.php'; ?>">Actualités</a>
+                </li>
+                <li>
                     <a href="<?php echo ROOT_URL . '/contact.php'; ?>">Contact</a>
                 </li>
                 <li>
